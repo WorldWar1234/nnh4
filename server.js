c89231db65cecb0c84552e4f6ed6aea2a7b42e49
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-03 13:36:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const compress = require('./src/compress');
 const shouldCompress = require('./src/shouldCompress');
+const redirect = require('./src/redirect'); 
 const bypass = require('./src/bypass');
-const redirect = require('./src/redirect');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -18,7 +18,12 @@ app.get('/', authenticate, params, (req, res) => {
     // Fetch the image from the URL
     request.get({ url, encoding: null }, (err, origin, buffer) => {
         if (err || origin.statusCode >= 400) {
-            // Use the redirect function if there is an error
+            return redirect(req, res);
+        }
+
+        if (origin.statusCode >= 300 && origin.headers.location) {
+            // Handle redirects
+            req.params.url = origin.headers.location;
             return redirect(req, res);
         }
 
@@ -28,7 +33,6 @@ app.get('/', authenticate, params, (req, res) => {
         if (shouldCompress(req)) {
             compress(req, res, buffer);
         } else {
-            // Bypass compression and directly send the original image
             bypass(req, res, buffer);
         }
     });
