4abba8aa83cf4dbd92d0649cba29484a6815c098
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-03 09:26:34 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,27 +1,30 @@
 const sharp = require('sharp');
-const redirect = require('./redirect');
 
 function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg';
+    const format = req.params.webp ? 'webp' : 'jpeg';
 
-  sharp(input)
-    .grayscale(req.params.grayscale)
-    .toFormat(format, {
-      quality: req.params.quality,
-      progressive: true,
-      optimizeScans: true
-    })
-    .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) {
-        return redirect(req, res);
-      }
+    sharp(input)
+        .grayscale(req.params.grayscale)
+        .toFormat(format, {
+            quality: req.params.quality,
+            progressive: true,
+            optimizeScans: true
+        })
+        .toBuffer((err, output, info) => {
+            if (err || !info || res.headersSent) {
+                return redirect(req, res);
+            }
 
-      res.setHeader('content-type', `image/${format}`);
-      res.setHeader('content-length', info.size);
-      res.setHeader('x-original-size', req.params.originSize);
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-      res.status(200).send(output);
-    });
+            res.setHeader('content-type', `image/${format}`);
+            res.setHeader('content-length', info.size);
+            res.setHeader('x-original-size', req.params.originSize);
+            res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+            res.setHeader('content-encoding', 'identity');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+            res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
+            res.status(200).send(output);
+        });
 }
 
 module.exports = compress;
