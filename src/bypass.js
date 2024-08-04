e7392241b86b6230fa7ec96d75017d509a750a88
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 11:48:37 +0530

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,10 +1,10 @@
-function bypass(req, res, buffer) {
-    res.setHeader('content-encoding', 'identity');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
-    res.setHeader('content-length', buffer.length);
-    res.status(200).send(buffer);
+function bypass(req, reply, buffer) {
+    reply.header('content-encoding', 'identity');
+    reply.header('Access-Control-Allow-Origin', '*');
+    reply.header('Cross-Origin-Resource-Policy', 'cross-origin');
+    reply.header('Cross-Origin-Embedder-Policy', 'unsafe-none');
+    reply.header('content-length', buffer.length);
+    reply.status(200).send(buffer);
 }
 
 module.exports = bypass;
