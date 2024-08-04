37d966377af9e8fd77eb5f5c7de1c40f03c4eb6d
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 15:46:45 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -2,22 +2,22 @@ const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desire
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 
 function shouldCompress(request) {
-    const { originType, originSize, webp } = request.query;
+  const { originType, originSize, webp } = request.params;
 
-    if (!originType.startsWith('image')) {
-        return false;
-    }
-    if (originSize === 0) {
-        return false;
-    }
-    if (webp && originSize < MIN_COMPRESS_LENGTH) {
-        return false;
-    }
-    if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
-        return false;
-    }
+  if (!originType.startsWith('image')) {
+    return false;
+  }
+  if (originSize === 0) {
+    return false;
+  }
+  if (webp && originSize < MIN_COMPRESS_LENGTH) {
+    return false;
+  }
+  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 module.exports = shouldCompress;
