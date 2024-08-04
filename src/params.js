c1f910bbd228c3906037d6e2afe210329b017f8a
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 19:49:57 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -5,7 +5,7 @@ function params(req, reply, next) {
 
     // Check if URL parameter is missing
     if (!url) {
-        return res.send('bandwidth-hero-proxy');
+        return reply.end('bandwidth-hero-proxy');
     }
 
     // Process parameters if URL is present
