09dc68996c68c25051e9e455efda4613782bd078
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 15:46:21 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -3,13 +3,19 @@ function redirect(request, reply) {
         return;
     }
 
-    reply.header('content-length', 0);
-    reply.header('location', encodeURI(request.query.url));
-    reply.header('content-encoding', 'identity');
-    reply.header('Access-Control-Allow-Origin', '*');
-    reply.header('Cross-Origin-Resource-Policy', 'cross-origin');
-    reply.header('Cross-Origin-Embedder-Policy', 'unsafe-none');
-    reply.status(302).send();
+    reply
+        .header('content-length', 0)
+        .removeHeader('cache-control')
+        .removeHeader('expires')
+        .removeHeader('date')
+        .removeHeader('etag')
+        .header('location', encodeURI(request.params.url))
+        .header('content-encoding', 'identity')
+        .header('Access-Control-Allow-Origin', '*')
+        .header('Cross-Origin-Resource-Policy', 'cross-origin')
+        .header('Cross-Origin-Embedder-Policy', 'unsafe-none')
+        .status(302)
+        .send();
 }
 
 module.exports = redirect;
