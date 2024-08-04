26507c5011cc8168725ae575fa10354680afe4cc
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 09:25:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.get('/', authenticate, params, (req, res) => {
         req.params.originType = origin.headers['content-type'] || '';
         req.params.originSize = buffer.length;
 
-        // Set common headers
+        /*/ Set common headers
         res.setHeader('content-encoding', 'identity');
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-        res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
+        res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none'); */
 
         if (shouldCompress(req)) {
             compress(req, res, buffer);
