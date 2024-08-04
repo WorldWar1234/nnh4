2834dc8f6e8152721c0c9d462d4b8871e9c69012
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 18:33:23 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,15 +1,15 @@
 const DEFAULT_QUALITY = 40;
 
-function params(req, res, done) {
-  let url = req.query.url;
-  if (!url) return res.send('bandwidth-hero-proxy');
+function params(request, reply, next) {
+  let url = request.query.url;
+  if (!url) return reply.send('bandwidth-hero-proxy');
 
-  req.params.url = decodeURIComponent(url);
-  req.params.webp = !req.query.jpeg;
-  req.params.grayscale = req.query.bw != 0;
-  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
+  request.params.url = decodeURIComponent(url);
+  request.params.webp = !request.query.jpeg;
+  request.params.grayscale = request.query.bw != 0;
+  request.params.quality = parseInt(request.query.l, 10) || DEFAULT_QUALITY;
 
-  done();
+  next();
 }
 
 module.exports = params;
