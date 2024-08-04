c2695b469c3e8fb252b753a917688a0907fc36d7
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 16:05:37 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,26 +1,26 @@
 const sharp = require('sharp');
 const redirect = require('./redirect');
 
-function compress(request, reply, input) {
-    const format = request.params.webp ? 'webp' : 'jpeg';
+function compress(req, res, input) {
+    const format = req.params.webp ? 'webp' : 'jpeg';
 
     sharp(input)
-        .grayscale(request.params.grayscale)
+        .grayscale(req.params.grayscale)
         .toFormat(format, {
-            quality: request.params.quality,
+            quality: req.params.quality,
             progressive: true,
             optimizeScans: true
         })
         .toBuffer((err, output, info) => {
-            if (err || !info || reply.headersSent) {
-                return redirect(request, reply);
+            if (err || !info || res.headersSent) {
+                return redirect(req, res);
             }
 
-            reply
+            res
                 .header('content-type', `image/${format}`)
                 .header('content-length', info.size)
-                .header('x-original-size', request.params.originSize)
-                .header('x-bytes-saved', request.params.originSize - info.size)
+                .header('x-original-size', req.params.originSize)
+                .header('x-bytes-saved', req.params.originSize - info.size)
                 .header('content-encoding', 'identity')
                 .status(200)
                 .send(output);
