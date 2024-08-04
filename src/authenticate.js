e34ae9eaf29e6e03c536a3046e642b1a2f4f43ae
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 16:07:06 +0530

    Update authenticate.js

--- src/authenticate.js
+++ src/authenticate.js
@@ -2,12 +2,12 @@ const auth = require('basic-auth');
 const LOGIN = process.env.LOGIN;
 const PASSWORD = process.env.PASSWORD;
 
-function authenticate(request, reply, done) {
+function authenticate(req, res, done) {
   if (LOGIN && PASSWORD) {
-    const credentials = auth(request);
+    const credentials = auth(req);
     if (!credentials || credentials.name !== LOGIN || credentials.pass !== PASSWORD) {
-      reply.header('WWW-Authenticate', `Basic realm="Bandwidth-Hero Compression Service"`);
-      reply.status(401).send('Access denied');
+      res.header('WWW-Authenticate', `Basic realm="Bandwidth-Hero Compression Service"`);
+      res.status(401).send('Access denied');
       return;
     }
   }
