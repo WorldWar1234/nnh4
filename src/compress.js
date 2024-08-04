091da3abeac4a8cb1e0c08bc6e3c5d2f5d7a58f2
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 18:34:43 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,30 +1,30 @@
 const sharp = require('sharp');
 const redirect = require('./redirect');
 
-function compress(req, res, input) {
-    const format = req.params.webp ? 'webp' : 'jpeg';
+function compress(request, reply, input) {
+  const format = request.params.webp ? 'webp' : 'jpeg';
 
-    sharp(input)
-        .grayscale(req.params.grayscale)
-        .toFormat(format, {
-            quality: req.params.quality,
-            progressive: true,
-            optimizeScans: true
-        })
-        .toBuffer((err, output, info) => {
-            if (err || !info || res.headersSent) {
-                return redirect(req, res);
-            }
+  sharp(input)
+    .grayscale(request.params.grayscale)
+    .toFormat(format, {
+      quality: request.params.quality,
+      progressive: true,
+      optimizeScans: true,
+    })
+    .toBuffer((err, output, info) => {
+      if (err || !info || reply.sent) {
+        return redirect(request, reply);
+      }
 
-            res
-                .header('content-type', `image/${format}`)
-                .header('content-length', info.size)
-                .header('x-original-size', req.params.originSize)
-                .header('x-bytes-saved', req.params.originSize - info.size)
-                .header('content-encoding', 'identity')
-                .status(200)
-                .send(output);
-        });
+      reply
+        .header('content-type', `image/${format}`)
+        .header('content-length', info.size)
+        .header('x-original-size', request.params.originSize)
+        .header('x-bytes-saved', request.params.originSize - info.size)
+        .header('content-encoding', 'identity')
+        .status(200)
+        .send(output);
+    });
 }
 
 module.exports = compress;
