cf19e4c22627fc14490e475e35ea6f2d2bdcf06b
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 16:06:40 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,15 +1,15 @@
-function redirect(request, reply) {
-    if (reply.headersSent) {
+function redirect(req, res) {
+    if (res.headersSent) {
         return;
     }
 
-    reply
+    res
         .header('content-length', 0)
         .removeHeader('cache-control')
         .removeHeader('expires')
         .removeHeader('date')
         .removeHeader('etag')
-        .header('location', encodeURI(request.params.url))
+        .header('location', encodeURI(req.params.url))
         .header('content-encoding', 'identity')
         .status(302)
         .send();
