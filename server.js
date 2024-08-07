72d2b6c29a76efae1ff77e851f495dfa6f925ece
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-07 19:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,64 +1,39 @@
-'use strict';
-
-const fastify = require('fastify')({
-  logger: true
-});
-const { request } = require('undici');
+const Fastify = require('fastify');
 const params = require('./src/params');
 const compress = require('./src/compress');
 const shouldCompress = require('./src/shouldCompress');
 const redirect = require('./src/redirect');
 
+const fastify = Fastify();
+const PORT = process.env.PORT || 8080;
+
 fastify.get('/', { preHandler: [params] }, async (req, reply) => {
-    const url = req.params.url;
+  const url = req.params.url;
 
-    try {
-        const { statusCode, headers, body } = await request(url, { method: 'GET' });
+  try {
+    const response = await fetch(url);
+    const buffer = await response.buffer();
 
-        // Check for successful image retrieval
-        if (statusCode >= 200 && statusCode < 300) {
-            req.params.originType = headers['content-type'] || '';
-            req.params.originSize = parseInt(headers['content-length'], 10);
+    req.params.originType = response.headers.get('content-type') || '';
+    req.params.originSize = buffer.length;
 
-            const buffer = Buffer.from(await body.arrayBuffer());
-
-            if (shouldCompress(req)) {
-                compress(req, reply, buffer);
-            } else {
-                redirect(req, reply);
-            }
-        } else {
-            // Handle errors (4xx and 5xx)
-            if (statusCode >= 400) {
-                return reply.status(500).send('Error fetching the image.');
-            }
-
-            // Handle redirections (3xx)
-            if (statusCode >= 300 && headers.location) {
-                req.params.url = headers.location;
-                return redirect(req, reply);
-            }
-
-            // If we reach this point, it means that the status code is not handled by any of the above conditions
-            // In this case, we should return a response to fulfill the promise
-            // This change is necessary to avoid the warning "Promise may not be fulfilled with 'undefined'" when the status code is not 204
-            return reply.status(statusCode).send();
-        }
-    } catch (error) {
-        console.error("Request error:", error);
-        return reply.status(500).send('Error fetching the image.');
+    if (shouldCompress(req)) {
+      compress(req, reply, buffer);
+    } else {
+      redirect(req, reply);
     }
+  } catch (error) {
+    console.error(error);
+    reply.status(500).send('Error fetching the image.');
+  }
 });
 
-fastify.get('/favicon.ico', (req, reply) => {
-  setImmediate(() => {
-    reply.status(204).send();
-  });
-  return reply;
-});
+fastify.get('/favicon.ico', (req, reply) => reply.status(204).send());
 
-// Updated fastify.listen() method to log the address that the server is listening on
-fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) throw err;
+fastify.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
   console.log(`Listening on ${address}`);
 });
